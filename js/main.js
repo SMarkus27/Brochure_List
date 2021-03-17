2e6b278ec18481d218f4d6fc6f65014b9ccf6355
SMarkus27 <pereira.markus.27@hotmail.com>
2021-03-17 17:42:04 -0300

    change the function to arrow functions

--- js/main.js
+++ js/main.js
@@ -1,69 +1,62 @@
-const sectionCenter = document.querySelector(".section-center");
-const container = document.querySelector(".btn-container");
+const btnContainer = document.querySelector(".btn-container");
+const sectionCenter = document.querySelector(".section-center");  
 
-// when the page loads, it shows all items
-window.addEventListener("load", function () {
+
+window.addEventListener('load',()=>{
   showBeerItems(beer);
   showBeerBtns();
+
 });
 
 const showBeerItems = (beerItems) => {
-
-
-  let showBeer = beerItems.map(function (item) {
-
+  let showBeer = beerItems.map((item)=>{
     return `<article class="beer-item">
-          <img src=${item.img} class="photo" alt=${item.title} />
-          <div class="item-info">
-            <header>
-              <h4>${item.title}</h4>
-            </header>
-            <p class="item-text">${item.desc}</p>
-          </div>
-            </article>`;
-    });
+    <img src=${item.img} class="photo" alt=${item.title} />
+    <div class="item-info">
+      <header>
+        <h4>${item.title}</h4>
+      </header>
+      <p class="item-text">${item.desc}</p>
+    </div>
+      </article>`;
 
+  });
   showBeer = showBeer.join("");
   sectionCenter.innerHTML = showBeer;
-} 
-
+}
 const showBeerBtns = () => {
-  const categories = beer.reduce(
-// all é setado como valor inicial, pois nao existe no beer
-    function (values, item) {
-      if (!values.includes(item.category)) {
-        values.push(item.category);
-      }
-      return values;
-    },
-    ["all"]
-  );
 
-// cria os botões conforme os valores obtidos na categories
-  const categoryBtns = categories.map(function (category) {
-      return `<button class="filter-btn" type="button" data-id=${category}>
-      ${category}</button>`;
-    }).join("");
-  container.innerHTML = categoryBtns;
-// filter-btns tem que ser selecionados depois que categoryBtns foi criado no HTML
-// ja que os botoes sao criados dinamicamente pelo JS 
-  const filterBtns = container.querySelectorAll(".filter-btn");
-  // filtra os itens
-  filterBtns.forEach(function (btn) {
-    btn.addEventListener("click", function (e) {
-      const category = e.currentTarget.dataset.id;
-// realiza um filtro para mostrar somente a categoria selecionada 
-      const beerCategory = beer.filter(function (beerItem) {
-    
-        if (beerItem.category === category) {
-          return beerItem;
-        }
-      });
-      if (category === "all") {
-        showBeerItems(beer);
-      } else {
-        showBeerItems(beerCategory);
+  const categories = beer.reduce((values,item) => {
+    if (!values.includes(item.category)) {
+      values.push(item.category);
+    }
+    return values;
+  },
+  ["all"]
+);
+  const categoryBtns = categories.map( (category) => {
+    return `<button class="filter-btn" type="button" data-id=${category}>
+    ${category}</button>`;
+  }).join("");
+  btnContainer.innerHTML = categoryBtns;
+
+const filterBtns = btnContainer.querySelectorAll(".filter-btn");
+filterBtns.forEach(function (btn) {
+  btn.addEventListener("click", (e) => {
+    const category = e.currentTarget.dataset.id;
+    const beerCategory = beer.filter( (beerItem) => {
+  
+      if (beerItem.category === category) {
+        return beerItem;
+        
       }
     });
+    if (category === "all") {
+      showBeerItems(beer);
+    } else {
+      showBeerItems(beerCategory);
+    }
   });
+});
+
 }
